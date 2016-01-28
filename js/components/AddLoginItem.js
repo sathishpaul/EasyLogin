8d0b858e28d6b7bbe9365bd9d3259bde9641428b
Sathish Paul Leo <sathish.paul.leo@oracle.com>
2016-01-28 08:05:50 -0800

    Add login item
    - Added boostrap buttons to launch dialog
    - Modal dialog contains just a simple attribute for now. Need to extend this to get all attributes needed to login

--- js/components/AddLoginItem.js
+++ js/components/AddLoginItem.js
@@ -5,13 +5,46 @@ import { Button } from 'react-bootstrap';
 var AddLoginItem = React.createClass({
 
   showAddDialog: function() {
-    console.log("hi");
+    console.log("trying to open");
+    $("#myModal").modal("show");
   },
 
   render: function() {
     return (
       <div className="addBtnContainer">
-        <Button bsStyle="success" onClick={this.showAddDialog}>Add Item</Button>
+        <button className="btn btn-success" data-target="#myModal" onClick={this.showAddDialog}>Add Item</button>
+
+        <div id="myModal" className="modal fade" tabIndex="-1" role="dialog">
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+                <h4 className="modal-title">Create a new Login Item</h4>
+              </div>
+              <div className="modal-body">
+                <div className="input-group">
+                  <span className="input-group-addon" id="sizing-addon2">URL</span>
+                  <input type="text" className="form-control" placeholder="http://your-web-app.com"
+                         aria-describedby="sizing-addon2" />
+                </div>
+
+                <div>
+                  <button type="button" className="btn btn-primary">Add attribute</button>
+                </div>
+                <div className="input-group">
+                  <span className="input-group-addon" id="sizing-addon2">@</span>
+                  <input type="text" className="form-control" placeholder="Username" aria-describedby="sizing-addon2" />
+                </div>
+              </div>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
+                <button type="button" className="btn btn-primary">Save changes</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
