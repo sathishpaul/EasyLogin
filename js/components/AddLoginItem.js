288e4d1a35bc2ffe18363e6d60130fbe6e05f863
Sathish Paul Leo <sathish.paul.leo@oracle.com>
2016-08-13 16:25:04 -0700

    Ability to add and remove attributes
    - This change introduces the add and remove buttons on each attribute row.
    - The form shows up with one attribute row (from initial state).
    - User can add or remove attribute rows except the last one. We need atleast one attribute row to function. Is this a valid assumption?
    - SVGs for add and remove buttons.

--- js/components/AddLoginItem.js
+++ js/components/AddLoginItem.js
@@ -5,9 +5,19 @@ import { Modal, ModalClose, ModalHeader, ModalBody, ModalTitle } from 'react-mod
 var AddLoginItem = React.createClass({
 
   getInitialState: function() {
+    var emptyAttribute = this._getEmptyAttributeModel();
     return {
       'isOpen': false,
-      'dialogMode': ''
+      'dialogMode': '',
+      'loginItem': {
+        'name': '',
+        'description': '',
+        'attributes': [
+          {
+            ...emptyAttribute
+          }
+        ]
+      }
     };
   },
 
@@ -43,6 +53,93 @@ var AddLoginItem = React.createClass({
 
   },
 
+  _getEmptyAttributeModel: function() {
+    return {
+      name: '',
+      value: '',
+      selector: 'id'
+    };
+  },
+
+  addNewAttributeRow: function() {
+    var newAttribute = this._getEmptyAttributeModel(),
+      loginItem = this.state.loginItem,
+      attributes = loginItem.attributes;
+    attributes.push(newAttribute);
+    this.setState({
+      loginItem: {
+        ...this.state.loginItem,
+        attributes: attributes
+      }
+    });
+  },
+
+  removeAttribute: function(index) {
+    if(index >=0 && this.state.loginItem.attributes.length > 1) {
+      var loginItem = this.state.loginItem,
+        attributes = loginItem.attributes.map((item) => item);
+
+      attributes.splice(index, 1);
+      loginItem.attributes = attributes;
+      this.setState({
+        loginItem: {
+          ...this.state.loginItem,
+          attributes: attributes
+        }
+      });
+      console.log("deleted");
+    } else {
+      console.log("cant delete the last item");
+    }
+  },
+
+  onChangeNameHandler: function(e) {
+    var nameValue = e.target.value;
+    this.setState({
+      loginItem: {
+        ...this.state.loginItem,
+        name: nameValue
+      }
+    });
+  },
+
+  onChangeDesc: function(e) {
+    var description = e.target.value;
+    this.setState({
+      loginItem: {
+        ...this.state.loginItem,
+        description: description
+      }
+    });
+  },
+
+  onChangeAttribute: function(key) {
+    /*
+     * Start here
+     *  - need to capture change of attribute per row and update model with key
+     *    figure out hwo to pass this function handler to onChange and receive key and newValue
+     *  - Need to add a submit action  with
+     *    - submit by clicking on button with id/class
+     *    - submit by hitting enter on attribute
+     *    - submit by invoking function?
+     * Then need to save
+     * */
+  },
+
+  renderAttributeRow: function(attributeRow, key) {
+    return (
+      <div className="form-group" key={key}>
+        <input type="text" className="form-control attributeRow" placeholder="Attribute name" autoComplete="off"
+               value={attributeRow.name} />
+        <input type="text" className="form-control attributeRow" placeholder="Attribute value" autoComplete="off"
+          value={attributeRow.value} />
+        <img src="/images/add.svg" className="attributeRowImg" onClick={this.addNewAttributeRow} />
+        <img src="/images/remove.svg" className="attributeRowImg leftSpacer"
+             onClick={this.removeAttribute.bind(this, key)} />
+      </div>
+    );
+  },
+
   renderDialog: function() {
     var dialogStyles = {
         base: {
@@ -51,37 +148,23 @@ var AddLoginItem = React.createClass({
         }
       };
 
-    /*
-     * Start here
-     *  - Add a +, - buttons to add or remove row
-     *  - Update model when new row is added
-     *  - Need to add a submit action  with
-     *    - submit by clicking on button with id/class
-     *    - submit by hitting enter on attribute
-     *    - submit by invoking function?
-     * Then need to save
-     * */
     return (
       <Modal isOpen={this.state.isOpen} onRequestHide={this.hideModal} dialogStyles={dialogStyles}>
         <ModalHeader>
           <ModalTitle>Create a new Login Item</ModalTitle>
         </ModalHeader>
         <ModalBody>
-
           <form autoComplete="off">
             <div className="form-group">
-              <input type="text" className="form-control" placeholder="Name to identify this item" autoComplete="off" />
+              <input type="text" className="form-control" placeholder="Name to identify this item"
+                     autoComplete="off" value={this.state.loginItem.name} onChange={this.onChangeNameHandler}/>
             </div>
             <div className="form-group">
-              <textarea rows="3" cols="5" className="form-control" placeholder="Short description" />
+              <textarea rows="3" cols="5" className="form-control" placeholder="Short description"
+                value={this.state.loginItem.description} onChange={this.onChangeDesc}/>
             </div>
             <p>Attributes</p>
-            <div className="form-group">
-              <img src="/images/addIcon.svg" width="32px" height="32px" />
-              <input type="text" className="form-control attributeRow" placeholder="Attribute name" autoComplete="off" />
-              <input type="text" className="form-control attributeRow" placeholder="Attribute value" autoComplete="off" />
-
-            </div>
+            {this.state.loginItem.attributes.map(this.renderAttributeRow)}
           </form>
         </ModalBody>
         <div className="modal-footer">
